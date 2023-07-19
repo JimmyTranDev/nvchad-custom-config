04b1c8487b788d337518b7c4abcd9fdc9af8a4c8
Jimmy Tran <jimmytrandev@gmail.com>
2023-07-19 21:42:57 +0200

    initial commit

---
+++ init.lua
@@ -0,0 +1,31 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+--
+
+
+-- Auto Yank
+local autocmd = vim.api.nvim_create_autocmd
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 500 }
+  end,
+})
+
+-- Go to definition and declaration
+vim.api.nvim_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+
+-- Custom settings
+vim.wo.relativenumber = true
+vim.opt.swapfile = false
+vim.opt.clipboard = "unnamedplus"
+
